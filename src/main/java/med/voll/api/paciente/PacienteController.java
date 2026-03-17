4c92fffa060198fcddbd6eee32d495529d6695c0
Caio <caiodesouza.cds@gmail.com>
2026-03-17 18:26:46 -0300

    Adicionado endpoint GET para pacientes

--- src/main/java/med/voll/api/paciente/PacienteController.java
+++ src/main/java/med/voll/api/paciente/PacienteController.java
@@ -2,11 +2,11 @@ package med.voll.api.paciente;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes")
@@ -20,5 +20,11 @@ public class PacienteController {
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados){
         repository.save(new Paciente(dados));
     }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listarPacientes(@PageableDefault(size=10, sort={"nome"}) Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
+    }
+
 
 }
