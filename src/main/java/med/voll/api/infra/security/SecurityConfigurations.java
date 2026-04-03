0718026c66c50f83676fa59667fdb6a6632dad62
Caio <caiodesouza.cds@gmail.com>
2026-04-03 14:18:48 -0300

    configurado tipo de autenticacao

---
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -0,0 +1,24 @@
+package med.voll.api.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    // aqui é um metodo para configurar a autenticacao
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        // aqui estou desabilitando proteção contra csrf, pois a autenticacao via token ja protege disso
+        return http.csrf(csrf -> csrf.disable())
+                // e aq estou dizendo "olha spring, nao quero usar o padrao stateful, com formulario, mas quero usar stateless"
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+}
